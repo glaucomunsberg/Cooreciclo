ffdc3570976f3eda78451bc3f2402364ad9d811c
glaucomunsberg <glaucomunsberg@gmail.com>
2012-02-24 17:50:37 -0200

    versão 0.0

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title id="title">correciclo</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href='http://fonts.googleapis.com/css?family=Reenie+Beanie' rel='stylesheet' type='text/css'>
+<link href="css/geral.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+
+<body>
+<table id="tabelaCorpo" width:"100%" height:"100%">
+  <tr>
+    <th id="tabelaCorpoColuna1" scope="col">
+    <p class="botao">
+	<a href="home.php" title="Meu Link">Botão CSS</a>
+	</p>
+    </th>
+    
+    <th id="tabelaCorpoColuna2" scope="col"><img src="imagens/meu_mundo.jpg" alt="" id="meu_mundo_imagem" />
+    </th>
+  </tr>
+</table>
+</body>
+</html>
